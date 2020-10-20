072a084e31d816d85f1180ed1ee88866874f2f01
Andrew Fuller <mactalla.obair@gmail.com>
2020-10-19 18:44:32 -0700

    Install NF Hack, if needed

--- install.sh
+++ install.sh
@@ -9,3 +9,12 @@ shopt -s extglob
 for file in .!(git|*.swp|.|); do
   [[ -e ~/${file} ]] || (echo "Linking ${file}" && ln -s $(pwd)/${file} ~/${file})
 done
+
+# Install Nerd Fonts
+function install_font() {
+    fc-list | ag "${1}.*Nerd Font.*ttf" > /dev/null && return 0
+    echo "Installing ${1} Nerd Font"
+    [[ -d ~/nerd-fonts ]] || git clone --depth 1 https://github.com/ryanoasis/nerd-fonts.git ~/nerd-fonts
+    sudo ~/nerd-fonts/install.sh -S "${1}"
+}
+install_font Hack
